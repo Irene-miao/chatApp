db121e69c1b7cbafe797d91b85d38dd8eedacdd5
Irene <irene_lee@live.com.sg>
2020-12-18 10:19:37 +0800

    Added input function

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "./screens/LoginScreen";
 import ChatScreen from "./screens/ChatScreen";
 
-
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 
 const Stack = createStackNavigator();
@@ -22,8 +12,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator headerMode="none" mode="modal">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Chat" component={ChatScreen} />
       </Stack.Navigator>
@@ -33,11 +22,4 @@ export default function App() {
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,48 +1,93 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Keyboard, TextInput,} from 'react-native';
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Keyboard,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function LoginScreen({ navigation }) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-return (
+  return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.container}>
-            <Text style={styles.title}>Chat App</Text>
-            <Text style={styles.fieldTitle}>Email</Text>
-            <TextInput
+      <View style={styles.container}>
+        <Text style={styles.title}>Chat App</Text>
+        <View style={styles.row}>
+          <Text style={styles.fieldTitle}>Email</Text>
+          <TextInput
             style={styles.input}
+            autoCapitalize="none"
             placeholder="Enter Email"
             value={email}
-            onChangeText={(text) => setEmail(text)} />
-            <Text style={styles.fieldTitle}>Password</Text>
-            <TextInput
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.fieldTitle}>Password</Text>
+          <TextInput
             style={styles.input}
+            autoCapitalize="none"
             placeholder="Enter Password"
             secureTextEntry={true}
             value={password}
-            onChangeText={(text) => setPassword(text)} />
-            <TouchableOpacity style={styles.logInButton} onPress={null}>
-                <Text style={styles.buttonText}>Log In</Text>
-            </TouchableOpacity>
+            onChangeText={(text) => setPassword(text)}
+          />
         </View>
+        <TouchableOpacity style={styles.logInButton} onPress={() => navigation.navigate("Chat")}>
+          <Text style={styles.buttonText}>Log In</Text>
+        </TouchableOpacity>
+      </View>
     </TouchableWithoutFeedback>
-);
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: [
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 30,
+    margin: 40,
+    fontWeight: "bold",
+  },
 
-    ],
-    fieldTitle: {
-        
-    }
-  });
+  fieldTitle: {
+    margin: 20,
+    fontSize: 20,
+  },
+
+  input: {
+    fontSize: 20,
+    margin: 10,
+    borderColor: "black",
+    borderRadius: 10,
+    borderWidth: 1,
+    
+  },
+
+  logInButton: {
+    backgroundColor: "blue",
+    borderRadius: 10,
+    padding: 10,
+    alignItems: "center",
+    width: 80,
+    height: 30,
+  },
+
+  buttonText: {
+    fontSize: 15,
+    color: "white",
+  },
+
+  row: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+  },
+});
